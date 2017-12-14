c3c36fd80ee4cb133e8222673ddc1b55c38ecc4e
7793dragons <34385263+7793dragons@users.noreply.github.com>
2017-12-13 20:58:00 -0600

    menus, camera, enemy Collider
    snapy stuff

--- Act In Space/Assets/Assets/Scripts/Camera/FollowPlayer.cs
+++ Act In Space/Assets/Assets/Scripts/Camera/FollowPlayer.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour {
 
 	public Transform player;
+
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		transform.position = player.position + new Vector3(0, 100, -50);
 	}
 }
